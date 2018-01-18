b1beb1e1fef53d69b8103fde8b3d7766ce07981e
Kevin <lopezkevin175@gmail.com>
2018-01-17 19:07:12 -0500

    organized and renamed files also installed sass

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/css/header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="portfolio-header">
+
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/css/index.css';
+import Header from './components/header';
+
+ReactDOM.render(<Header />, document.getElementById('root'));
