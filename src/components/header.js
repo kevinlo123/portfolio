3eb62478669e75ff14aec5f0d116c2dce95c7105
Kevin <lopezkevin175@gmail.com>
2018-01-20 13:33:54 -0500

    finishes developing header and hero-banner components - also made responsive

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import '../styles/css/header.css';
 
 class Header extends Component {
+
+    showNav(){
+        const navMobile = document.getElementsByClassName("header-compact-nav-mobile")[0];
+        const blackOverLay = document.getElementsByClassName("black-overlay")[0];
+        const hamburgerMenu = document.getElementById("mobile-nav-icon");
+        const mobileNavItems = document.getElementsByTagName("ul")[1];
+
+        hamburgerMenu.classList.toggle("hamburger-clicked");
+        mobileNavItems.classList.toggle("show-nav-items");
+        console.log("hamburger menu has been clicked");   
+        
+        if(hamburgerMenu.classList.contains("hamburger-clicked")){
+            blackOverLay.style.display = "block";  
+        }else{
+            blackOverLay.style.display = "none";              
+        }
+    }
+
     render() {
         return (
         <header className="portfolio-header">
@@ -12,6 +30,22 @@ class Header extends Component {
                     <li className="nav-list-item"><a href="#" className="list-item-anchor">Projects</a></li>
                     <li className="nav-list-item"><a href="#" className="list-item-anchor">Contact</a></li>
                 </ul>
+            </nav>
+            <nav>
+                <div className="header-compact-nav-mobile">
+                    <a onClick={this.showNav} className="header-compact-nav-icon" id="mobile-nav-icon">
+                        <span className="header-compact-nav-icon-line line-1"></span>
+                        <span className="header-compact-nav-icon-line line-2"></span>
+                        <span className="header-compact-nav-icon-line line-3"></span>
+                    </a>
+                    <ul>
+                        <li className="mobile-nav-items"><a href="#" className="list-item-anchor-mobile">Home</a></li>
+                        <li className="mobile-nav-items"><a href="#" className="list-item-anchor-mobile">About</a></li>
+                        <li className="mobile-nav-items"><a href="#" className="list-item-anchor-mobile">Projects</a></li>
+                        <li className="mobile-nav-items"><a href="#" className="list-item-anchor-mobile">Contact</a></li>
+                    </ul>
+                    <div className="black-overlay"></div>
+                </div>
             </nav>
         </header>
         );
