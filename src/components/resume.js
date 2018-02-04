cc220b4dd839452b5d3ea6d6bb4b87f5cb82af5f
Kevin <lopezkevin175@gmail.com>
2018-02-04 07:04:19 -0500

    developed the resume CTA button

--- src/components/resume.js
+++ src/components/resume.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import '../styles/scss/resume.scss';
 
 class Resume extends Component {
+
+    makeRed(){
+        document.getElementsByClassName("resume-link")[0].style.color = "#ff003b";
+    }
+
+    makeWhite(){
+        document.getElementsByClassName("resume-link")[0].style.color = "#fff";
+    }
+
     render(){
         return(
             <div className="resume-container">
-                <h1 className="checkout-text">Check out my Resume!</h1>
-                <button>Resume</button>
+                <button onMouseEnter={this.makeRed} onMouseLeave={this.makeWhite} className="resume-button">
+                    <a className="resume-link" href="./RESUME.pdf">Resume</a>
+                </button>
             </div>
         );
     }
